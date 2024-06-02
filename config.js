78455106facb6df21207c7c92f7bc2b432166e5a
Livzjasteb <zyxsesi@gmail.com>
2024-06-02 14:12:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,18 +6,18 @@ const readMore = more.repeat(4001)
 
 // Owner
 global.owner = [
-['628xxx', 'Xyroinee', true]
+['6283176793414', 'Xyroinee', true]
 ]
 global.mods = []
 global.prems = []
 // Info
-global.nomorwa = '628xxx'
-global.packname = 'anu - MD by'
-global.author = '© Xyroinee'
-global.namebot = '​anu - MD'
-global.wm = '© anu By Xyroine'
-global.stickpack = 'anu - MD by'
-global.stickauth = '© Xyroine'
+global.nomorwa = '6283176793414'
+global.packname = 'Zyx - MD by'
+global.author = '© Zyxx'
+global.namebot = '​Zyxx - MD'
+global.wm = '© Zyxx By Jii'
+global.stickpack = 'Zyx - MD by'
+global.stickauth = '© jimalesin'
 global.fotonya = 'https://telegra.ph/file/83b522fd80b660705d1cd.jpg'
 // Link Sosmed
 global.sig = 'https://www.instagram.com/xyroinee'
@@ -32,9 +32,9 @@ global.wait = 'Sedang Di Proses, Mohon Tunggu....'
 global.eror = 'Terjadi Kesalahan Coba Lagi Nanti!'
 global.multiplier = 69 
 // Apikey
-global.lol = 'apikeymu'
-global.rose = 'apikeymu'
-global.xyro = 'apikeymu'
+global.lol = 'ZaSAFxtqrPRix2L'
+global.rose = 'ZaSAFxtqrPRix2L'
+global.xyro = 'ZaSAFxtqrPRix2L'
 // Catatan : Jika Mau Work Fiturnya
 // Masukan Apikeymu
 // Gapunya Apikey? Ya Daftar
@@ -48,7 +48,7 @@ global.APIs = {
 
 /Apikey/
 global.APIKeys = {
-    "https://api.xyroinee.xyz": "apikeymu",
+    "https://api.xyroinee.xyz": "ZaSAFxtqrPRix2L",
 }
 
 let file = fileURLToPath(import.meta.url)
